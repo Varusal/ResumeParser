3312dcf95a248fa224f7d4d131d14fb8c28a92d4
Varusal <10616926+Varusal@users.noreply.github.com>
2025-07-27 11:00:41 +0200

    Initial commit and working prototype

---
+++ Classes/FileHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+namespace ResumeParser.Classes
+{
+    internal class FileHandler
+    {
+        private string pdfContent;
+
+        public FileHandler()
+        {
+            pdfContent = string.Empty;
+        }
+
+        public string GetResumeFile(string path)
+        {
+            Console.Clear();
+
+            PdfDocument pdfDoc = PdfDocument.Open(path);
+            int pdfPageCount = pdfDoc.NumberOfPages;
+
+            for (int i = 1; i <= pdfDoc.NumberOfPages; i++)
+            {
+                Page pdfPage = pdfDoc.GetPage(i);
+                pdfContent += pdfPage.Text;
+            }
+
+            return pdfContent;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ResumeParser.Classes;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata;
+using System.Text;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+namespace ResumeParser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            string apiKey = string.Empty;
+            string model = "gpt-4.1-nano";
+            string uri = "chat/completions";
+            string prompt = "Summarize the following job applicaton: ";
+            string pdfContent = string.Empty;
+            string userPrompt = string.Empty;
+            string chatComplID = string.Empty;
+
+            FileHandler fhandler = new FileHandler();
+
+            Console.Write("Enter api key: ");
+            apiKey = Console.ReadLine();
+
+            client.BaseAddress = new Uri("https://api.openai.com/v1/");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+            Console.Clear();
+
+            Console.Write("Enter path to job application (pdf): ");
+            prompt += fhandler.GetResumeFile(Console.ReadLine());
+
+            Console.Clear();
+
+            
+
+            do
+            {
+                if (userPrompt != string.Empty)
+                {
+                    prompt = "Job Application: " + pdfContent + " " + userPrompt;
+                }
+
+                var requestBody = new
+                {
+                    model = model,
+                    messages = new[]
+                    {
+                        new { role = "user", content = prompt }
+                    },
+                    store = true
+                };
+
+                StringContent content = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
+
+                System.Threading.Tasks.Task<HttpResponseMessage> response = client.PostAsync(uri, content);
+
+                if (response != null)
+                {
+                    string responseString = response.Result.Content.ReadAsStringAsync().Result;
+
+                    JObject data = (JObject)JsonConvert.DeserializeObject(responseString);
+                    string message = data["choices"].First()["message"]["content"].ToString();
+
+                    Console.WriteLine(message);
+                }
+
+                Console.Write("> ");
+                userPrompt = Console.ReadLine();
+
+            } while (userPrompt != string.Empty);
+
+            Console.Write("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
