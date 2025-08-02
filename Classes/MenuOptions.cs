a5a9f937ec81454462b16371a0895aa8cdf4c6bd
Varusal <10616926+Varusal@users.noreply.github.com>
2025-08-02 19:46:47 +0200

    Starting to implement main menu

---
+++ Classes/MenuOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResumeParser.Classes
+{
+    internal class MenuOptions
+    {
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public Action Method { get; set; }
+
+        public MenuOptions()
+        {
+            Name = string.Empty;
+            Message = string.Empty;
+            Method = () => { };
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿using ResumeParser.Classes;
+using System.Data;
 using System.Net.Http.Headers;
+using System.Net.WebSockets;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using UglyToad.PdfPig.Fonts.TrueType.Names;
 
 namespace ResumeParser
 {
@@ -8,6 +13,7 @@ namespace ResumeParser
         // Todo: Add menu options
         static void Main(string[] args)
         {
+            List<MenuOptions> menu = new List<MenuOptions>();
             HttpClient client = new HttpClient();
 
             string fileContent = string.Empty;
@@ -18,11 +24,34 @@ namespace ResumeParser
             FileHandler fHandler = new FileHandler();
             AIHandler aiHandler = new AIHandler();
             HTTPHandler httpHandler = new HTTPHandler();
+            
 
             aiHandler.Model = "gpt-4.1-nano";
             aiHandler.Uri = "chat/completions";
             aiHandler.Prompt = "Summarize the following job applicaton: ";
             aiHandler.BaseURI = new Uri("https://api.openai.com/v1/");
+
+            CreateMenu(menu);
+            #region Menu testing
+            // To do: loop indefinitely until user exits
+            bool renderMenu = true;
+            do
+            {
+                for (int i = 0; i < menu.Count; i++)
+                {
+                    Console.WriteLine(i + ". " + menu[i].Message);
+                }
+
+                Console.WriteLine();
+                Console.Write("Select menu option: ");
+                int menuChoice = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                menu[menuChoice].Method.Invoke();
+
+            } while (renderMenu);
+            #endregion
+
 
             do
             {
@@ -86,6 +115,32 @@ namespace ResumeParser
 
             Console.Write("Press any key to exit...");
             Console.ReadLine();
+        }
+
+        private static void CreateMenu(List<MenuOptions> menu)
+        {
+            MenuOptions menuOption = new MenuOptions
+            {
+                Name = "APIKey",
+                Message = "Change API Key.",
+                Method = () => { }
+            };
+
+            menu.Add(menuOption);
+        }
+
+        private static void ChangeAPIKey(AIHandler aiHandler)
+        {
+            do
+            {
+                Console.Write("Enter api key: ");
+                aiHandler.ApiKey = Console.ReadLine();
+                if (string.IsNullOrEmpty(aiHandler.ApiKey))
+                {
+                    Console.WriteLine("API key cannot be empty. Please try again.");
+                    Console.WriteLine();
+                }
+            } while (string.IsNullOrEmpty(aiHandler.ApiKey));
         }
     }
 }
